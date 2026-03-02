8e188987ad682a02730af55ec7f2d5984a52df37
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-03-02 18:20:28 +0530

    removes json handling from the agent

--- src/agent.js
+++ src/agent.js
@@ -10,35 +10,11 @@ export const runAgent = async (messages) => {
   while (true) {
     logger(["CALLING LLM...."]);
     const res = await callLLM(messages, tools);
-    // let toolCalls = res.tool_calls || [];
+    const toolCalls = res.tool_calls || [];
 
     logger(["MESSAGE", messages]);
     logger(["RESPONSE", res]);
 
-    if (
-      toolCalls.length === 0 && content.includes('"name":') &&
-      content.includes('"parameters":')
-    ) {
-      try {
-        const jsonMatch = content.match(/\{[\s\S]*\}/); //matches the open and close braces
-        if (jsonMatch) {
-          const parsedJSON = JSON.parse(jsonMatch);
-          if (parsedJSON.name && parsedJSON.parameters) {
-            toolCalls = [{
-              function: {
-                name: parsedJSON.name,
-                arguments: parsedJSON.parameters,
-              },
-            }];
-            res.content = "";
-          }
-        }
-      } catch {
-        logger(["Failed to parse the hallucinated JSON: ", error.message]);
-      }
-    }
-
-    const toolCalls = res.message.tool_calls || [];
     logger(["TOOLS CALL", toolCalls]);
     messages.push(res.message);
 
@@ -53,5 +29,5 @@ export const runAgent = async (messages) => {
     await executeToolCall(toolCall, messages);
   }
 
-  return messages.at(-1)?.content;
+  return messages.at(-1).content;
 };
